1fdcfb115c5a72da1b03770165431b9338add4f7
Brunno Oliveira <brunnokick@gmail.com>
2023-02-26 20:05:40 -0300

    novo arquivo

--- src/b3/acoes.py
+++ src/b3/acoes.py
@@ -29,7 +29,7 @@ class Acoes:
     def _load_data(self):
         data_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
         b3_posicao = os.path.join(data_path, "b3_posicao")
-        b3_arquivo = os.path.join(b3_posicao, "posicao-2023-02-17.xlsx")
+        b3_arquivo = os.path.join(b3_posicao, "posicao-2023-02-24.xlsx")
 
         print(f"Loading {b3_arquivo}")
         self.df = pd.read_excel(b3_arquivo, sheet_name="Acoes")
